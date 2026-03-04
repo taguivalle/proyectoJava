75a9d432e030793ed42e1999f82e9cbe23d640f8
hector gustavo vallejo espinosa <taguivalle140965@gmail.com>
2026-03-04 22:31:23 +0100

    Hice unas lineas sencillas de código

---
+++ src/main/java/com/gustavo/Proyecto.java
@@ -0,0 +1,10 @@
+package com.gustavo;
+
+/**
+ * Clase de ejemplo dentro del paquete com.gustavo
+ */
+public class Proyecto {          // ← Mayúscula inicial
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde el paquete com.gustavo!");
+    }
+}
